91f14ecfc1d6854ba4e9bd668d6daba4bdacb204
tanfan <tanfan@luojilab.com>
2019-01-03 16:11:38 +0800

    完善命令行

---
+++ utils/index.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const fetch = require('node-fetch')
+const fs = require('fs')
+const request = require('request');
+const spin = require('io-spin')
+
+const getApiUrl =  function(page){
+  return `https://m.weibo.cn/api/container/getIndex?containerid=2304132948117283_-_WEIBO_SECOND_PROFILE_WEIBO&page_type=01&page=${page}`
+}
+
+/**
+ * 获取视频
+ * @param {page} page 
+ * @param {number} number 
+ */
+const getList = function(page = 1){
+  return new Promise(async (resolve, reject) => {
+    let result = []
+    let num = 0
+    const spinner = spin('Fetching data')
+    spinner.start()
+    let { data } = await fetch(getApiUrl(page)).then(res => res.json())
+    spinner.stop()
+    if(data.cards.length > 0){
+      data.cards.forEach(ele => {
+        if(ele.card_type === 9 && ele.mblog !== undefined && ele.mblog.page_info !== undefined &&  ele.mblog.page_info.type === "video" && /\[第(\d)+次\]/.test(ele.mblog.page_info.media_info.next_title)){
+          let title = ele.mblog.page_info.media_info.next_title.match(/第(\d)+次/)[0]
+          let url = ele.mblog.page_info.media_info.mp4_720p_mp4
+          result.push({
+            title: title,
+            url: url
+          })
+          num ++
+        }
+      })
+    }
+    resolve({
+      data: result,
+      total: num
+    })
+  })
+}
+
+/**
+ * 下载视频
+ * @param {url} url 
+ * @param {title} name 
+ */
+const downloadVideo = function (options){
+  let stream = fs.createWriteStream(path.join(__dirname, `../dist/video/${options.title}.mp4`))
+  request(options.url).pipe(stream)
+    .on('error', () => {
+      console.log(`视频${options.title}下载失败...`)
+    })
+    .on('close', () => {
+    console.log(`视频${options.title}下载完成...`)
+  }); 
+}
+
+module.exports = {
+  getList,
+  downloadVideo,
+}
